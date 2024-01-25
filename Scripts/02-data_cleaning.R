f09c981bb60c04c13b6a1306bd147d2f503f171e
MeerSisodia1707 <meersisodia170703@gmail.com>
2024-01-24 20:08:55 -0500

    Update commit

--- Scripts/02-data_cleaning.R
+++ Scripts/02-data_cleaning.R
@@ -12,24 +12,31 @@ library(lubridate)
 #### Clean and Prepare data ####
 raw_data <- read_csv("Inputs/data/raw_data.csv")
 
-clean_data <- raw_data |> select(TFS_Alarm_Time)
-year_data <- clean_data |> mutate(TFS_Alarm_Time = factor(year(TFS_Alarm_Time)))
-year_data <- year_data |> count(TFS_Alarm_Time)
-year_data <- year_data |> rename(year = TFS_Alarm_Time)
+clean_data <- raw_data |> select(TFS_Alarm_Time, Civilian_Casualties, Ignition_Source)
+clean_data <- clean_data |> mutate(year = factor(year(TFS_Alarm_Time)), month = month(TFS_Alarm_Time))
+clean_data <- clean_data |> select(-TFS_Alarm_Time)
+clean_data
+year_data <- clean_data |> select(year)
+year_data <- year_data |> count(year)
 year_data <- year_data |> rename(incidents = n)
 year_data
 
 
-month_data <- clean_data |> mutate(TFS_Alarm_Time = month(TFS_Alarm_Time))
-month_data <- month_data |> count(TFS_Alarm_Time)
-month_data <- month_data |> rename(month = TFS_Alarm_Time)
+month_data <- clean_data |> select(month)
+month_data <- month_data |> count(month)
 month_data <- month_data |> rename(incidents = n)
-month_data <- month_data |>
-  mutate(month = month(month, label = TRUE,abbr = TRUE))
+month_data <- month_data |> mutate(month = month(month, label = TRUE,abbr = TRUE))
 month_data
+
+clean_sample_data <- head(clean_data, 5)
+raw_data_selected <- raw_data |> select(TFS_Alarm_Time, Civilian_Casualties, Ignition_Source)
+sample_data <- head(raw_data_selected, 5)
+
 
 #### Save data ####
 
 write_csv(year_data, "Outputs/data/year_data.csv")
 write_csv(month_data, "Outputs/data/month_data.csv")
+write_csv(sample_data, "Outputs/data/sample_data.csv")
+write_csv(clean_sample_data, "Outputs/data/clean_sample_data.csv")
 
